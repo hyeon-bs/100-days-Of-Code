5d36062019b151b517abf39d00285d47c41d6c03
BSH <bshyeon1217@gmail.com>
2025-02-23 17:30:48 +0900

    feat: Data Types

---
+++ Day 2/Data Types/task.py
@@ -0,0 +1,15 @@
+# Subscripting
+print("Hello"[0])
+
+# String
+print("123" + "345")
+
+# Integer = Whole number
+print(123 + 345)
+
+# Large Integers
+print(123,456,789)
+
+# Boolean
+print(True)
+print(False)
