bf953dfe0a7edeccf16e071da2b86546137210f7
sohyeon <88962154+hyeon-bs@users.noreply.github.com>
2025-08-19 13:52:57 +0900

    Add day-30

---
+++ Day-30/main.py
@@ -0,0 +1,25 @@
+# FileNotFound
+
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key": "value"}
+#     print(a_dictionary["Key"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist.")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     raise TypeError("This is an error that I made up.")
+
+height = float(input("Heigh: "))
+weight = int(input("Weigh: "))
+
+if height > 3:
+    raise ValueError("Human Height should not be over 3 meters.")
+
+bmi = weight / height ** 2
+print(bmi)
